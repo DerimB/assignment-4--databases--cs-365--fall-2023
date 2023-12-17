b860405313febd9b6b25a01fce78beb5ec8bee91
DerimB <123917611+DerimB@users.noreply.github.com>
2023-12-17 13:57:20 -0500

    Updated Keys

--- mysql/create-tables.sql
+++ mysql/create-tables.sql
@@ -8,7 +8,8 @@ CREATE TABLE IF NOT EXISTS album (
   artist_id  INT          NOT NULL,
   album_id   INT          NOT NULL,
   album_name VARCHAR(128) NOT NULL,
-  PRIMARY KEY (artist_id, album_id)
+  PRIMARY KEY (album_id),
+  FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
 );
 
 CREATE TABLE IF NOT EXISTS track (
@@ -17,10 +18,12 @@ CREATE TABLE IF NOT EXISTS track (
   track_id   INT          NOT NULL,
   track_name VARCHAR(128) NOT NULL,
   time       DECIMAL(5,2) NOT NULL,
-  PRIMARY KEY (artist_id, album_id, track_id)
+  PRIMARY KEY (track_id),
+  FOREIGN KEY (album_id) REFERENCES album(album_id)
 );
 
 CREATE TABLE IF NOT EXISTS played (
   played     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (played)
+  FOREIGN KEY (track_id) REFERENCES Track(track_id)
 );
