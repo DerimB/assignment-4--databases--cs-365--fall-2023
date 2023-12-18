4e6f6b878782f8f4417d3db95316533f64598e2d
DerimB <123917611+DerimB@users.noreply.github.com>
2023-12-18 12:01:14 -0500

    Update create-tables.sql

--- mysql/create-tables.sql
+++ mysql/create-tables.sql
@@ -6,24 +6,28 @@ CREATE TABLE IF NOT EXISTS artist (
 
 CREATE TABLE IF NOT EXISTS album (
   artist_id  INT          NOT NULL,
-  album_id   INT          NOT NULL,
+  album_id   INT          NOT NULL AUTO_INCREMENT,
   album_name VARCHAR(128) NOT NULL,
+  INDEX (artist_id),
   PRIMARY KEY (album_id),
-  FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
+  FOREIGN KEY (artist_id) REFERENCES artist(artist_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS track (
   artist_id  INT          NOT NULL,
   album_id   INT          NOT NULL,
-  track_id   INT          NOT NULL,
+  track_id   INT          NOT NULL AUTO_INCREMENT,
   track_name VARCHAR(128) NOT NULL,
   time       DECIMAL(5,2) NOT NULL,
+  INDEX (album_id),
   PRIMARY KEY (track_id),
-  FOREIGN KEY (album_id) REFERENCES album(album_id)
+  FOREIGN KEY (album_id) REFERENCES album(album_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS played (
   played     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (played)
-  FOREIGN KEY (track_id) REFERENCES Track(track_id)
+  track_id   INT          NOT NULL,
+  INDEX (track_id),
+  PRIMARY KEY (played),
+  FOREIGN KEY (track_id) REFERENCES track(track_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
