6ce8d18fd316ac1c084d2f49d71052e43d28c988
DerimB <123917611+DerimB@users.noreply.github.com>
2023-12-19 14:12:10 -0500

    Deleted comments
    README states to have commands separated by a new line.

--- commands.sql
+++ commands.sql
@@ -1,10 +1,7 @@
---The Concat function is used to generate the predicted outcome as stated in the README
 SELECT CONCAT(artist_name, ' recorded ', album_name) AS Recording
 FROM artist
 JOIN album ON artist.artist_id = album.artist_id;
 
---The select statement was fixed to ensure the two attributed shown in the example output were given.
---Also the query was complete using a nested query.
 SELECT artist_name, album_name
 FROM artist
 JOIN album ON artist.artist_id = album.artist_id
@@ -15,17 +12,14 @@ WHERE artist.artist_id IN (
     WHERE artist.artist_id = album.artist_id
 );
 
---Uses the Compiles relation between the artist and album tables, This gets the output of the artist_name and album_name attributes as requested/shown.
 SELECT artist.artist_name, album.album_name
 FROM artist
 JOIN album ON artist.artist_id = album.artist_id;
 
---Uses the Contains relation between the album table and the track table.
 SELECT album.album_name, track.track_name
 FROM album
 JOIN track ON album.album_id = track.album_id;
 
---Uses the WasPlayedAt relation between the track table and the played table.
 SELECT track.track_name, track.time
 FROM track
 JOIN played ON track.track_id = played.track_id;
